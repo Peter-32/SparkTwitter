7a4b82d9eaddc8c73727609465c68e2b97a69f2b
Peter-32 <peter@impactradius.com>
2017-07-04 18:37:44 -0700

    a

--- src/main/scala/io/github/peter32/spark/FirstMapReduce.scala
+++ src/main/scala/io/github/peter32/spark/FirstMapReduce.scala
@@ -11,14 +11,14 @@ object FirstMapReduce {
     val sc = new SparkContext(conf)
 
     // The number 2 is the minimum number of processes running at once.
-    val input = sc.textFile("/Users/peterjmyers/Documents/Projects/SparkTwitter/dictionary.txt", 2)
+    val input = sc.textFile("/Users/peterjmyers/Documents/Projects/SparkTwitter/src/main/resources/dictionary.txt", 2)
     val words = input.flatMap(line => line.split(" ")).flatMap(line => line.split("-"))
     val result = words.map(word => (word, word.length) // Get word lengths
     ).reduceByKey { case (x, y) => (x) // Remove duplicates
     }.map { case (word, length) => (length, word) // swap key and value so length is the new key for sorting
     }.sortByKey(false, 1 // sorting descending by length
     ).map { case (length, word) => word } // remove the length part
-    result.saveAsTextFile("dictionary_sorted_by_length.txt")
+    result.saveAsTextFile("data_output/dictionary_sorted_by_length")
     sc.stop()
   }
 }
