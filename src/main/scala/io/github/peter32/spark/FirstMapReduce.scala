80342b28a56bbc4ad8750a4647358fd743879b56
Peter-32 <peter@impactradius.com>
2017-07-05 21:55:59 -0700

    adding documentation

--- src/main/scala/io/github/peter32/spark/FirstMapReduce.scala
+++ src/main/scala/io/github/peter32/spark/FirstMapReduce.scala
@@ -7,21 +7,38 @@ import java.io.File
 
 object FirstMapReduce {
   def main(args: Array[String]) {
+    // The input file is the README.md to this project.
+    // Get the absolute path to this input file.
+    val inputFile = (new File("src/main/resources/dictionary.txt")).getAbsolutePath
+    // Delete the output file if it exists.  Otherwise returns false and doesn't throw an error.
+    new File("data_output/dictionary_sorted_by_length").delete()
+    // You get a conf and sc variable when starting a Spark application.
+    // Getting a SparkConf and calling it conf.  Set the app name.
     val conf = new SparkConf().setAppName("wordCount")
+    // Getting a SparkContext and calling it sc.
     val sc = new SparkContext(conf)
-    val f = new File("src/main/dictionary.txt")
-    // The number 2 is the minimum number of processes running at once.
-    val input = sc.textFile(f.getAbsolutePath, 2)
-    // Split a text file by spaces and hyphens
-    val words = input.flatMap(line => line.split(" ")).flatMap(line => line.split("-"))
-    val result = words.map(word => (word, word.length) // Get word lengths
-    ).reduceByKey { case (x, y) => (x) // Remove duplicates
-    }.map { case (word, length) => (length, word) // swap key and value so length is the new key for sorting
-    }.sortByKey(false, 1 // sorting descending by length
-    ).map { case (length, word) => word } // remove the length part
-
-    // END
+    // Pass in the input file.  The 2 is the minimum number of parallel processes to use.
+    val input = sc.textFile(inputFile, 2)
+    // Chain the RDD operations together.
+    // Variable "result" defines lazy transformations, no data is computed on yet.
+      // Split a text file into words by spaces.
+    val result = input.flatMap(line => line.split(" ")
+      // Get word lengths.  The RDD is now a key-value pair RDD.
+    ).map(word => (word, word.length)
+      // Remove duplicate keys.
+    ).reduceByKey { case (x, y) => (x)
+      // Swap key and value so length is the new key.
+      // Length is the key so we can sort on it.
+    }.map { case (word, length) => (length, word)
+      // Sorting by length.
+      // Passing in false means sort descending.
+    }.sortByKey(false, 1
+      // Turn the key-value pair RDD into an RDD.
+      // Keep the word and drop the length.
+    ).map { case (length, word) => word }
+    // Function saveAsTextFile executes the transformation and saves the RDD to a file.
     result.saveAsTextFile("data_output/dictionary_sorted_by_length")
+    // Tell the spark context to stop.
     sc.stop()
   }
 }
--- src/main/scala/io/github/peter32/spark/HelloWorld.scala
+++ src/main/scala/io/github/peter32/spark/HelloWorld.scala
@@ -7,14 +7,24 @@ import java.io.File
 
 object HelloWorld {
   def main(args: Array[String]) {
-    val f = new File("README.md")
-    val logFile = f.getAbsolutePath // Should be some file on your system
+    // The input file is the README.md to this project.
+    // Get the absolute path to this input file.
+    val inputFile = (new File("README.md")).getAbsolutePath
+    // You get a conf and sc variable when starting a Spark application.
+    // Getting a SparkConf and calling it conf.  Set the app name.
     val conf = new SparkConf().setAppName("Simple Application")
+    // Getting a SparkContext and calling it sc.
     val sc = new SparkContext(conf)
-    val logData = sc.textFile(logFile, 2).cache()
+    // Pass in the input file.  The 2 is the minimum number of parallel processes to use.
+    // Cache is explained here.  https://spark.apache.org/docs/latest/quick-start.html
+    val logData = sc.textFile(inputFile, 2).cache()
+    // Filter for only the rows that contain a, then store the count to a variable
     val numAs = logData.filter(line => line.contains("a")).count()
+    // Filter for only the rows that contain b, then store the count to a variable
     val numBs = logData.filter(line => line.contains("b")).count()
+    // Print the results to the console.
     println(s"Hello World: Lines with a: $numAs, Lines with b: $numBs")
+    // Tell the spark context to stop.
     sc.stop()
   }
 }
